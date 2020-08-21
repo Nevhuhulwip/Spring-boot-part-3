5d3399804c35a9a4c51b8d8369dab455a86e2f9e
Nevhuhulwip <pfanonevhuhulwi@gmail.com>
2020-08-21 15:39:28 +0200

    second attempt

---
+++ src/main/java/com/pfano/springbootpart3/SpringBootPart3Application.java
@@ -0,0 +1,13 @@
+package com.pfano.springbootpart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPart3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPart3Application.class, args);
+	}
+
+}
